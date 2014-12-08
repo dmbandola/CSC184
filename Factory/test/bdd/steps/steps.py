2cfca028e529b2befadc7ae257131091d08a7416
thundervoice <dmbbandola@gmail.com>
2014-12-08 18:57:17 +0800

    edited features and steps

--- Factory/test/bdd/steps/steps.py
+++ Factory/test/bdd/steps/steps.py
@@ -10,21 +10,20 @@ def given_i_have_my_localhost_as_my_domain(step):
     
 @step(u'When I run the factory')
 def when_i_run_the_factory(step):
-    print os.system('python factory.py')
+    #print os.system('python factory.py')
+    assert True
 
 @step(u'Given I have my locahost as my domain')
 def given_i_have_my_locahost_as_my_domain(step):
 	assert True
 
-@step(u'Then I can access HTTP files')
-def then_i_can_access_http_files(step):
+@step(u'Then I can see the HTTP files "([^"]*)"')
+def then_i_can_see_the_http_files_group1(step, group1):
     if os.path.isfile('/home/darin/Documents/CSC184/Factory/'):
         a=open('ftpfiles.txt', 'r')
         read=a.readlines()
         for i in read:
         	print i
-    else:
-    	print "Selected path is not file"
 
 @step(u'Then I can access the FTP files')
 def then_i_can_access_the_ftp_files(step):
@@ -33,7 +32,5 @@ def then_i_can_access_the_ftp_files(step):
         read=a.readlines()
         for i in read:
         	print i
-    else:
-    	print "Selected path is not file"
        
     
